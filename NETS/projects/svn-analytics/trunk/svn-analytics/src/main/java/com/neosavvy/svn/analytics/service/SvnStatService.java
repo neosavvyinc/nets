f14f759f6afdf7935d96f0f08f42e8dc8295cfe5
aparrish <aparrish@neosavvy.com>
2009-04-08 01:16:24 +0000

    Adding in pom generate hooks for automagic AS from Java generation. Checking in generated AS files. Hooking into two new service methods to refine searchees.

--- NETS/projects/svn-analytics/trunk/svn-analytics/src/main/java/com/neosavvy/svn/analytics/service/SvnStatService.java
+++ NETS/projects/svn-analytics/trunk/svn-analytics/src/main/java/com/neosavvy/svn/analytics/service/SvnStatService.java
@@ -4,12 +4,17 @@ import com.neosavvy.svn.analytics.dto.Author;
 import com.neosavvy.svn.analytics.dto.HistoricalTeamStatistic;
 import com.neosavvy.svn.analytics.dto.OverallTeamStatistic;
 import com.neosavvy.svn.analytics.dto.SVNRepositoryInterval;
+import com.neosavvy.svn.analytics.dto.request.RefineSearchRequest;
 
 public interface SvnStatService {
 
 	public OverallTeamStatistic[] getOverallTeamStatistics();
+	
+	public OverallTeamStatistic[] getRefinedTeamStatistics(RefineSearchRequest request);
 
 	public HistoricalTeamStatistic[] getHistoricalTeamStatistics();
+	
+	public HistoricalTeamStatistic[] getRefinedHistoricalTeamStatistics(RefineSearchRequest request);
 
 	public Author[] getAuthors();
 
--- NETS/projects/svn-analytics/trunk/svn-analytics/src/main/java/com/neosavvy/svn/analytics/service/SvnStatServiceImpl.java
+++ NETS/projects/svn-analytics/trunk/svn-analytics/src/main/java/com/neosavvy/svn/analytics/service/SvnStatServiceImpl.java
@@ -1,37 +1,55 @@
 package com.neosavvy.svn.analytics.service;
 
+import org.apache.log4j.Logger;
+
 import com.neosavvy.svn.analytics.dao.SVNStatisticDAO;
 import com.neosavvy.svn.analytics.dto.Author;
 import com.neosavvy.svn.analytics.dto.HistoricalTeamStatistic;
 import com.neosavvy.svn.analytics.dto.OverallTeamStatistic;
 import com.neosavvy.svn.analytics.dto.SVNRepositoryInterval;
+import com.neosavvy.svn.analytics.dto.request.RefineSearchRequest;
 
 public class SvnStatServiceImpl implements SvnStatService {
 
-    private SVNStatisticDAO dao;
+	private static final Logger logger = Logger
+			.getLogger(SvnStatServiceImpl.class);
 
-    public SVNStatisticDAO getDao() {
-        return dao;
-    }
+	private SVNStatisticDAO dao;
 
-    public void setDao(SVNStatisticDAO dao) {
-        this.dao = dao;
-    }
+	public SVNStatisticDAO getDao() {
+		return dao;
+	}
 
-    public OverallTeamStatistic[] getOverallTeamStatistics() {
-        return dao.getOverallTeamStats();
-    }
+	public void setDao(SVNStatisticDAO dao) {
+		this.dao = dao;
+	}
 
-    public HistoricalTeamStatistic[] getHistoricalTeamStatistics() {
-        return dao.getHistoricalTeamStats();
-    }
+	public OverallTeamStatistic[] getOverallTeamStatistics() {
+		return dao.getOverallTeamStats();
+	}
 
-    public Author[] getAuthors() {
-        return dao.getAuthors();
-    }
+	public HistoricalTeamStatistic[] getHistoricalTeamStatistics() {
+		return dao.getHistoricalTeamStats();
+	}
 
-    public SVNRepositoryInterval getRepositoryInterval() {
-    	return dao.getRepositoryInterval();
-    }
-    
+	public Author[] getAuthors() {
+		return dao.getAuthors();
+	}
+
+	public SVNRepositoryInterval getRepositoryInterval() {
+		return dao.getRepositoryInterval();
+	}
+
+	public HistoricalTeamStatistic[] getRefinedHistoricalTeamStatistics(
+			RefineSearchRequest request) {
+		logger.info("Testing historical refine request");
+		return null;
+	}
+
+	public OverallTeamStatistic[] getRefinedTeamStatistics(
+			RefineSearchRequest request) {
+		logger.info("Testing overall refine request");
+		return null;
+	}
+
 }
