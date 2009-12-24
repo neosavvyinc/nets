f1bdb505cb23b508022a3306ec1d4b3b58d8e275
aparrish <aparrish@neosavvy.com>
2009-12-24 14:52:52 +0000

    Adding a few database initialize methods. 1) When the server starts a database initializer bean will fire off truncates to clean up anything that was inserted previously and also insert a ROLE since it is required to properly save a user. 2) When the tests run the database script in webapps can execute during the process-test-resources phase via the SQL Maven plugin.
    Also checking in the FlexMonkey tests I have written.

---
+++ NETS/projects/commons-user/trunk/commons-user-java-lib/src/main/java/com/neosavvy/user/util/DatabaseInitializerListener.java
@@ -0,0 +1,63 @@
+package com.neosavvy.user.util;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.servlet.ServletContextListener;
+import javax.sql.DataSource;
+/*************************************************************************
+ *
+ * NEOSAVVY CONFIDENTIAL
+ * __________________
+ *
+ *  Copyright 2008 - 2009 Neosavvy Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Neosavvy Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Neosavvy Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Neosavvy Incorporated.
+ **************************************************************************/
+
+/**
+ * User: adamparrish
+ * Date: Dec 24, 2009
+ * Time: 9:15:30 AM
+ */
+public class DatabaseInitializerListener implements InitializingBean {
+
+    public static Logger logger = Logger.getLogger(DatabaseInitializerListener.class);
+
+    private JdbcTemplate template;
+
+    public void afterPropertiesSet () {
+
+        try {
+            template.execute("TRUNCATE TABLE COMPANY");
+            template.execute("TRUNCATE TABLE NUM_EMPLOYEES_RANGE");
+            template.execute("TRUNCATE TABLE ROLE");
+            template.execute("TRUNCATE TABLE USER");
+            template.execute("TRUNCATE TABLE USER_COMPANY_ROLE");
+            template.execute("TRUNCATE TABLE USER_INVITE");
+            template.update("INSERT INTO ROLE(LONG_NAME,SHORT_NAME) VALUES (?, ?)",new Object[]{"Administrator","ROLE_ADMIN"});
+        } catch (DataAccessException e) {
+            logger.error(e);
+        }
+        
+    }
+
+    public DataSource getDataSource() {
+        return template.getDataSource();
+    }
+
+    public void setDataSource( DataSource dataSource ) {
+        this.template = new JdbcTemplate( dataSource );
+    }
+}
