6bf2bb1e4cb53f59457698d8cf9b7b8ca61fe491
aparrish <aparrish@neosavvy.com>
2012-01-13 04:46:37 +0000

    Updating problems with production build

--- NETS/projects/commons-user/tags/NETS-1.0/restore-database.sh
+++ NETS/projects/commons-user/tags/NETS-1.0/restore-database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #run as postgres user
 #./recreate-database.sh
-psql --username=commonsuser commonsuser < prod-backups/03-19-2011.sql
+psql --username=commonsuser commonsuser < prod-backups/01-12-2012.sql
